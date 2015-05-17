7bdb03c74f1da3ec33af854dd63610fd676d7d4c
gonni <gonni21c@gmail.com>
2015-05-17 13:44:45 +0900

    Intial Commit and Push

---
+++ study_git_01/src/com/ygsoft/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ygsoft.test;
+
+public class HelloWorld {
+	
+	public static void main(String ... v) {
+		System.out.println("Hello Git .. 0001");
+		
+		System.out.println("Hello Git .. 0002");
+		
+		System.out.println("Hello Git .. 0003");
+	}
+}

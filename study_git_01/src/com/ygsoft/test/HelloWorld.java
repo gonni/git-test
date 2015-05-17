721db54077f25a31c686f5d78013a5944e4620bb
jeff <jeff@localhost>
2015-05-17 15:25:32 +0900

    This is update test2 (changing an existing context line)

--- study_git_01/src/com/ygsoft/test/HelloWorld.java
+++ study_git_01/src/com/ygsoft/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String ... v) {
 		System.out.println("Hello Git .. 0001");
 		
-		System.out.println("Hello Git .. 0002");
+		System.out.println("Hello Git .. 0005");
 		System.out.println("This file is just updated .. 0004");
 		System.out.println("Hello Git .. 0003");
 	}

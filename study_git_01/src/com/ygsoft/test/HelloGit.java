00fdd6216597766cd431ac6ea29c4429ee5131a5
jeff <jeff@localhost>
2015-05-17 14:25:05 +0900

    This is a second TEST

---
+++ study_git_01/src/com/ygsoft/test/HelloGit.java
@@ -0,0 +1,9 @@
+package com.ygsoft.test;
+
+public class HelloGit {
+	
+	public static void main(String ... v) {
+		System.out.println("Hello GIT !!");
+		System.out.println("This File is newly adder to test git repository 0001");
+	}
+}

f41422eede7302fbfe411eb8aa9fc268653df4ed
gonni <gonni21c@gmail.com>
2015-05-17 14:59:32 +0900

    This is update test

--- study_git_01/src/com/ygsoft/test/HelloWorld.java
+++ study_git_01/src/com/ygsoft/test/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello Git .. 0001");
 		
 		System.out.println("Hello Git .. 0002");
-		
+		System.out.println("This file is just updated .. 0004");
 		System.out.println("Hello Git .. 0003");
 	}
 }
